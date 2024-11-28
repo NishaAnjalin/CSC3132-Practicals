183fae70da9487ccc8b066e86b6957016441f24c
Nishaanjalin2023 <139215902+Nishaanjalin2023@users.noreply.github.com>
2024-11-28 23:41:57 +0000

    Connection file Created

--- Public/20.11.2024/dbconf.php
+++ Public/20.11.2024/dbconf.php
@@ -1,5 +1,5 @@
 <?php
-//constant variable (variable name, value
+//constant variable (variable name, value)
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
