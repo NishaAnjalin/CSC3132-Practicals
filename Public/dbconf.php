6a7a61ed09fc7fadab4f3ccbdd814e254b223e9b
Nishaanjalin2023 <139215902+Nishaanjalin2023@users.noreply.github.com>
2024-11-28 12:17:05 +0000

    Connection code added

--- Public/dbconf.php
+++ Public/dbconf.php
@@ -1,6 +1,6 @@
 <?php
 //constant variable (variable name, value)
-define('SERVERNAME', '127');
+define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
 define('DBNAME', 'school');
