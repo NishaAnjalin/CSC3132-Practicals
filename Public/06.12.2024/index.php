63cb5ec17239d4d0cedba8ef1f6ed52a7cd7d31a
Nishaanjalin2023 <139215902+Nishaanjalin2023@users.noreply.github.com>
2024-12-06 11:24:37 +0000

    add the index file

---
+++ Public/06.12.2024/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nishanthi Practicals</h1><br><br>
+
+    <a href="gettable.php">Gettable</a><br>
+    <a href="printtable.php">Printtable</a><br>
+    <a href="myfunc.php">MyFunction</a><br>
+
+
+    
+</body>
+</html>
