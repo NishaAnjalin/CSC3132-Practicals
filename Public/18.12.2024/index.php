558256cada7e2a43e576034aba2f270d664d6300
Nishaanjalin2023 <nishaanjalin.com>
2024-12-18 15:07:23 +0530

    updated

--- Public/18.12.2024/index.php
+++ Public/18.12.2024/index.php
@@ -9,7 +9,7 @@
     <h1>Nishanthi Practicals</h1><br><br>
 
     <a href="gettable.php">Gettable</a><br>
-    //<a href="printtable.php">printtable</a><br>
+    <a href="printtable.php">printtable</a><br>
 
     
 </body>
