de3523b669d6b82a63a57d2fb8bdeab33516b447
Nishaanjalin2023 <nishaanjalin.com>
2024-12-18 15:06:13 +0530

    print the table

--- Public/18.12.2024/printtable.php
+++ Public/18.12.2024/printtable.php
@@ -10,10 +10,17 @@
 require_once 'dbconf.php'; 
 require_once 'myfunc.php';
 
-$book_id = $_GET['book_id'];
+if (isset($_GET['book_id']) && $_GET['book_id'] != '') {
+    $book_id = $_GET['book_id'];
 
-Libdetails($book_id,$connect);
-//Jointable($book_id, $connect);
+    // Check if the view is for authors
+    if (isset($_GET['view']) && $_GET['view'] === 'authors') {
+        Jointable($book_id, $connect); // Display author details
+    } else {
+        Libdetails($book_id, $connect); // Display book details
+    }
+}
+
  
 
 ?>
